e09fb54f6ba2edd7de788c6be3aa09fa27e26a73
shruti240104 <shrutisingh240504@gmail.com>
2026-05-24 23:51:10 +0530

    Set up Clerk authentication integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
+import { ClerkProvider } from '@clerk/react'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>,
+  <ClerkProvider>
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </ClerkProvider>,
 )
