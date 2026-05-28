68c1c63936649de8b17a41045268711aa83849ef
shruti240104 <shrutisingh240504@gmail.com>
2026-05-28 19:42:04 +0530

    Implemented job listing section with reusable job cards

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import {createContext,useState} from "react";
+import {createContext,useState,useEffect} from "react";
+import {jobsData} from '../assets/assets'
 
 export const AppContext = createContext()
 
@@ -12,11 +13,25 @@ export const AppContextProvider = (props) => {
 
   const [isSearched,setIsSearched] = useState(false)
 
+  const [jobs,setJobs] = useState([])
+
+  //Function to fetch job data
+
+  const fetchJobs = async () => {
+    setJobs(jobsData)
+  }
+
+  useEffect(()=>{
+    fetchJobs()
+  },[])
+
   const value = {
     searchFilter,
     setSearchFilter,
     isSearched,
-    setIsSearched
+    setIsSearched,
+    jobs,
+    setJobs
   }
   return (
     <AppContext.Provider value={value}>
