63b70f148e56f154daa2730ec2dc19638015ebf6
shruti240104 <shrutisingh240504@gmail.com>
2026-06-03 18:56:37 +0530

    Implemented resume view and edit functionality on applications page

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Navbar from '../components/Navbar';
+import {assets} from '../assets/assets'
+
 
 const Applications = () => {
+
+  const [isEdit,setIsEdit] = useState(false)
+  const [resume,setResume] = useState(null)
+
   return (
-    <div>Applications</div>
+    <>
+    <Navbar />
+    <div className='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10'>
+      <h2 className='text-xl font-semibold'>Your Resume</h2>
+      <div className='flex gap-2 mb-6 mt-3'>
+        {
+          isEdit ? 
+          <>
+            <label className='flex items-center' htmlFor="resumeUpload">
+              <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2'>Select Resume</p>
+              <input onChange={e=>setResume(e.target.files[0])} accept='application/pdf' type="file" hidden id="resumeUpload"/>
+              <img src={assets.profile_upload_icon}alt=""/>
+            </label>
+            <button className='bg-green-100 border border-green-400 rounded-lg px-4 py-2' onClick={e=>setIsEdit(false)}>Save</button>
+          </>
+          : <div className='flex gap-2'>
+            <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href="">
+              Resume
+            </a>
+            <button onClick={()=>setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2'>
+              Edit
+            </button>
+          </div>
+        }
+      </div>
+    </div>
+    </>
   )
 }
 
