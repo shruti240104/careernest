586c5d5543904997758576e1e5aaa07999287008
shruti240104 <shrutisingh240504@gmail.com>
2026-06-04 23:47:42 +0530

    Implemented recruiter dashboard navbar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { Outlet,useNavigate } from 'react-router-dom';
+import { assets } from '../assets/assets'
 
 const Dashboard = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <Outlet />
+    <div className='min-h-screen'>
+      {/* Navbar for Recruiter Panel */}
+      <div className='shadow py-4'>
+        <div className='px-5 flex justify-between items-center'>
+          <img onClick={e=>navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
+          <div className='flex items-center gap-3'>
+            <p className='max-sm:hidden'>Welcome, Shruti</p>
+            <div className='relative group'>
+              <img className='w-8 rounded-full' src={assets.company_icon} alt="" />
+              <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                <ul className='list-none m-0 p-2 bg-white rounded-md text-sm'>
+                  <li className='py-1 px-2 cursor-pointer pr-10'>
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
