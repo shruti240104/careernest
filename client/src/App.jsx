12f942d23083a59ca78b6582a4ab2ee996779753
shruti240104 <shrutisingh240504@gmail.com>
2026-06-04 23:21:33 +0530

    Set up recruiter dashboard routing and nested routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,10 @@ import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
 import RecruiterLogin from "./components/RecruiterLogin";
 import {AppContext} from './context/AppContext'
-
+import Dashboard from "./pages/Dashboard"
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplications from "./pages/ViewApplications";
 const App = () => {
   const {showRecruiterLogin} = useContext(AppContext)
   return (
@@ -14,7 +17,12 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/apply-job/:id' element={<ApplyJob/>}/>
-        <Route path='/applications' element={<Applications/>} />
+        <Route path='/applications' element={<Applications />} />
+        <Route path='/dashboard' element={<Dashboard />} >
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='manage-jobs' element={<ManageJobs />}/>
+          <Route path='view-applications' element={<ViewApplications />}/>
+        </Route>
       </Routes>
     </div>
   )
---
+++ client/src/pages/AddJob.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const AddJob = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default AddJob
