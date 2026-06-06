9552655049f892338af0f7dfc50ce419ef4d6d70
shruti240104 <shrutisingh240504@gmail.com>
2026-06-06 17:33:04 +0530

    Integrated Sentry error monitoring in backend

---
+++ server/config/instrument.js
@@ -0,0 +1,33 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+
+Sentry.init({
+  dsn: "https://4c388060b214ea3c071bb3544e569003@o4511518228676608.ingest.us.sentry.io/4511518234705920",
+  integrations: [
+    nodeProfilingIntegration(),
+    Sentry.mongooseIntegration()
+  ],
+
+  // Send structured logs to Sentry
+  enableLogs: true,
+  // Tracing
+  //tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: 'trace',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
+// Profiling happens automatically after setting it up with `Sentry.init()`.
+// All spans (unless those discarded by sampling) will have profiling data attached to them.
+Sentry.startSpan({
+  name: "My Span",
+}, () => {
+  // The code executed here will be profiled
+});
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
+import * as Sentry from "@sentry/node";
 
 //intialize express
 const app = express()
@@ -16,9 +18,14 @@ app.use(express.json())
 
 //Routes
 app.get('/',(req,res)=>res.send("API Working"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
 //Port
 const PORT = process.env.PORT || 5000
+
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`)
