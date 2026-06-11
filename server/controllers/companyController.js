c8000424dd101aa2cf2eeb99e96ad780cff39e9f
shruti240104 <shrutisingh240504@gmail.com>
2026-06-11 21:30:13 +0530

    feat: implement company jobs retrieval and job visibility update

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -145,6 +145,18 @@ export const getCompanyJobApplicants = async (req, res) => {
 //Get Company Posted Jobs
 export const getCompanyPostedJobs = async (req, res) => {
 
+  try{
+    const companyId = req.company._id
+    const jobs = await Job.find({companyId})
+
+    //(toDo) adding no. of applicants info in data
+
+    res.json({success:true,jobsData:jobs})
+  }
+  catch(error){
+    res.json({success:false,message:error.message})
+  }
+
 }
 
 // Change Job Application Status
@@ -154,5 +166,23 @@ export const changeJobApplicationStatus = async (req, res) => {
 
 // Change job visibility
 export const changeJobVisibility = async (req, res) => {
+  try{
+    const {id} = req.body
 
+    const companyId = req.company._id
+
+    const job = await Job.findById(id)
+
+    if(companyId.toString() === job.companyId.toString()){
+      job.visible = !job.visible
+    }
+
+    await job.save()
+
+    res.json({success:true,job})
+
+  }
+  catch(error){
+    res.json({success:false,message:error.message})
+  }
 }
