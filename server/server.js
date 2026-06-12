0ba7eb7dbdf1b1ddeec0b8db1b1a9d1605b073b3
shruti240104 <shrutisingh240504@gmail.com>
2026-06-12 23:36:20 +0530

    feat: integrate Clerk authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import { clerkWebHooks } from './controllers/webhooks.js'
 import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 //intialize express
 const app = express()
@@ -20,6 +22,7 @@ await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 //Routes
 app.get('/',(req,res)=>res.send("API Working"))
@@ -29,6 +32,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 app.post('/webhooks',clerkWebHooks)
 app.use('/api/company',companyRoutes)
 app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
 
 
 //Port
