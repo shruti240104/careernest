f4e3cd7de868622be3f4ea2ab852ca9f5f62b335
shruti240104 <shrutisingh240504@gmail.com>
2026-06-17 17:43:24 +0530

    feat: implement recruiter logout functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,7 +7,15 @@ const Dashboard = () => {
 
   const navigate = useNavigate()
 
-  const { companyData } = useContext(AppContext)
+  const { companyData,setCompanyData,setCompanyToken } = useContext(AppContext)
+
+  //function to logout for company
+  const logOut = () => {
+    setCompanyToken(null)
+    localStorage.removeItem('companyToken')
+    setCompanyData(null)
+    navigate('/')
+  }
 
   return (
     <div className='min-h-screen'>
@@ -22,7 +30,7 @@ const Dashboard = () => {
                 <img className='w-8 rounded-full' src={companyData.image} alt="" />
                 <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                   <ul className='list-none m-0 p-2 bg-white rounded-md text-sm'>
-                    <li className='py-1 px-2 cursor-pointer pr-10'>
+                    <li onClick={logOut}className='py-1 px-2 cursor-pointer pr-10'>
                       Logout
                     </li>
                   </ul>
